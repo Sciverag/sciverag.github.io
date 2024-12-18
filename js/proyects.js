b8ecdb9274d5988511605559db01f681d89c882a
Samuel <samuelciga2004@gmail.com>
2024-12-19 00:08:19 +0100

    Finished the proyects page

--- js/proyects.js
+++ js/proyects.js
@@ -1,13 +1,17 @@
 const proyectConatainer = document.getElementById("proyectContainer");
+const proyectsBackground = document.getElementById("proyectsBackground");
+const proyectsBackgroundBottom = document.getElementById("proyectsBackgroundBottom");
 const proyectsList = document.getElementById("proyectsList");
 const selectedProyect = document.getElementById("selectedProyect");
 const proyectTitle = document.getElementById("proyectTitle");
+const proyectsTitle = document.getElementById("proyectsTitle");
 const pName = document.getElementById("name");
 const pType = document.getElementById("type");
 const proyectTechs = document.getElementById("proyectTechs");
 const proyectDescription = document.getElementById("proyectDescription");
 const proyectImage = document.getElementById("proyectImage");
 const closeButton = document.getElementById("close");
+const backButton  = document.getElementById("backButton");
 
 fetch("../info/proyects.json")
 .then(result => result.json())
@@ -22,8 +26,10 @@ closeButton.addEventListener("click", () => {
     proyectDescription.setAttribute("class","reverse");
     proyectImage.setAttribute("class","reverse");
     closeButton.setAttribute("class","reverse");
+    backButton.removeAttribute("class");
     proyectTitle.style.display = "none";
     proyectTechs.style.display = "none";
+    backButton.style.display = "none";
     proyectDescription.style.display = "none";
     proyectImage.style.display = "none";
     closeButton.style.display = "none";
@@ -34,11 +40,36 @@ closeButton.addEventListener("click", () => {
         proyectDescription.style.display = "flex";
         proyectImage.style.display = "block";
         closeButton.style.display = "block";
+        backButton.style.display = "block";
     },1)
 
     setTimeout(() => {
         selectedProyect.style.display = "none";
     },750)
+});
+
+backButton.addEventListener("click", () => {
+    proyectsBackground.setAttribute("class","reverse");
+    proyectsBackgroundBottom.setAttribute("class","reverse");
+    proyectsList.setAttribute("class","reverse");
+    backButton.setAttribute("class","reverse");
+    proyectsTitle.setAttribute("class","reverse");
+    proyectsBackground.style.display = "none";
+    proyectsBackgroundBottom.style.display = "none";
+    proyectsList.style.display = "none";
+    backButton.style.display = "none";
+    proyectsTitle.style.display = "none";
+    setTimeout(() => {
+        proyectsBackground.style.display = "block";
+        proyectsBackgroundBottom.style.display = "block";
+        proyectsList.style.display = "block";
+        backButton.style.display = "block";
+        proyectsTitle.style.display = "block";
+    },1);
+
+    setTimeout(() => {
+        window.location.href = "home.html";
+    }, 1000)
 })
 
 function showProyects(proyects){
@@ -72,7 +103,9 @@ function showProyects(proyects){
             })
 
             proyectsList.setAttribute("class","reverse");
+            backButton.setAttribute("class","reverse")
             proyectsList.style.display = "none";
+            backButton.style.display = "none";
             proyectTitle.removeAttribute("class");
             proyectTechs.removeAttribute("class");
             proyectDescription.removeAttribute("class");
@@ -81,6 +114,7 @@ function showProyects(proyects){
             setTimeout(() => {
                 proyectsList.style.display = "block";
                 selectedProyect.style.display = "grid";
+                backButton.style.display = "block";
             },1)
 
             setTimeout(() => {
